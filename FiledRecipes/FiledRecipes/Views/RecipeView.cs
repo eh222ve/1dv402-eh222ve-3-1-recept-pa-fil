83d10d3ebec5462e3c2bba200240930861610a26
eh222ve <eh222ve@student.lnu.se>
2014-10-15 12:11:49 +0200

    Styling of recipe output

--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -12,5 +12,59 @@ namespace FiledRecipes.Views
     /// </summary>
     public class RecipeView : ViewBase, IRecipeView
     {
+
+        public void Show(IRecipe recipe, bool clear = true)
+        {
+            if (clear)
+            {
+                Console.Clear();
+            }
+
+            Header = recipe.Name;
+            ShowHeaderPanel();
+
+            Console.WriteLine(Strings.Ingredients);
+            for (int i = 0; i < Strings.Ingredients.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+
+            foreach (IIngredient ing in recipe.Ingredients)
+            {
+                Console.WriteLine(ing.ToString());
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(Strings.Instructions);
+            for (int i = 0; i < Strings.Instructions.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+
+            int j = 1;
+
+            foreach (string ins in recipe.Instructions)
+            {
+                Console.WriteLine("<{0}>", j++);
+                Console.WriteLine(ins.ToString());
+
+            }
+		}
+
+
+
+        public void Show(IEnumerable<IRecipe> recipes)
+        {
+            foreach (IRecipe recipe in recipes)
+            {
+                Show(recipe);
+                this.ContinueOnKeyPressed();
+                Console.Clear();
+            }
+
+        }
     }
 }
